8301dd9fbe1dda302aa02285fa17a1fba5a2c134
bhaveshpp <bhaveshp.magento@gmail.com>
2022-02-11 17:44:20 +0530

    add doc comment and formate composer.json

--- registration.php
+++ registration.php
@@ -4,10 +4,17 @@
  *
  * This code is developed by Bhavesh Prajapati
  *
- * Do not edit or add to this file if you wish to upgrade this extension to newer
- * version in the future.
+ * Do not edit or add to this file, a composer will
+ * revert changes while upgrading.
+ * 
+ * if you need new features or found any bug
+ * you can suggest us here.
+ * https://github.com/bhaveshpp/magento2-module-base/issues
  *
- * @authors https://github.com/bhaveshpp
+ * @category Magento2
+ * @package  Bhaveshpp_Base
+ * @author Bhavesh Prajapati <bhaveshp.magento@gmail.com>
+ * @license  https://bhaveshpp.github.io/LICENSE.txt
  */
 use Magento\Framework\Component\ComponentRegistrar;
 ComponentRegistrar::register(
